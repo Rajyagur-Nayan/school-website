54610ac2d341d5bc5e3f91cd8f41ec6d32ea5080
Rajyaguru Nayan <nayanrajyaguru59@gamil.com>
2025-10-28 21:49:25 +0530

    add path

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ initializeBirthdayScheduler();
 //middlewear
 app.use(
   cors({
-    origin: "https://school-management-vert-chi.vercel.app", // your frontend URL
+    origin: "https://school-management-beige-three.vercel.app", // your frontend URL
     credentials: true, // allow cookies
   })
 );
