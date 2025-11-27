6bccb682e0200c65f1e906ad856309b228e422bd
Rajyaguru Nayan <nayanrajyaguru59@gamil.com>
2025-11-27 14:58:32 +0530

    path error resolve

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: [
       "https://school-website-nine-psi.vercel.app",
-      "https://admin-smv-nine.vercel.app",
+      "https://admin-smv-topaz.vercel.app",
       "https://student-smv.vercel.app",
       "http://localhost:3000",
     ], // your frontend URL
