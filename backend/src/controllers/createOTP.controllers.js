a1e50e6e03d33f4aaaa9349af235ab7516a699a2
Dhruv Boghani <dhruvboghani624@gmail.com>
2025-11-28 18:19:18 +0530

    Dhruv:code clean

--- backend/src/controllers/createOTP.controllers.js
+++
@@ -1,9 +0,0 @@
-function generateOTP(limit) {
-    var digits = '0123456789';
-    let OTP = '';
-    for (let i = 0; i < limit; i++) {
-        OTP += digits[Math.floor(Math.random() * 10)];
-    }
-    return OTP;
-}
-module.exports = generateOTP;
